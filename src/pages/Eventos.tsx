ac7398590a60b7a6e9d3b642c86c968bfb8997a3
Adrian <adrian@tecnosolve.com.br>
2026-01-06 11:45:05 -0300

    correção para deploy

--- src/pages/Eventos.tsx
+++ src/pages/Eventos.tsx
@@ -575,7 +575,6 @@ export default function Eventos() {
                     startTime: '',
                     endDate: '',
                     endTime: '',
-                    quantity: '',
                     ticketTypeId: '',
                   })
                 }}
